f90a1d3a6d29cfc90fdf0cd6145d1c4d511b0316
Ahmed Gamal <agmghazi@hotmail.com>
2020-02-22 13:02:31 +0300

    change browser zoom

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 app.use(express.static(__dirname + "/"));
 
-app.listen(6060,()=>{
-    console.log('app live on port 6060');
+app.listen(9090,()=>{
+    console.log('app live on port 9090');
     
 })
